c869aed017c08a73ea9e084a21a26831c30689b6
ShikharThaQuri <shikharchand4321@gmail.com>
2025-06-03 14:54:29 +0545

    don't know change

--- style.js
+++ style.js
@@ -10,40 +10,37 @@ const image8 = document.getElementById("image8");
 // const imgs = document.querySelectorAll(".imgs");
 // const gallery = document.getElementById("gallery");
 
-// const observer = new IntersectionObserver(
-//   (entries) => {
-//     entries.forEach((entry) => {
-//       if (entry.isIntersecting) {
-//         image1.classList.add("image1");
-//         image2.classList.add("image2");
-//         image3.classList.add("image3");
-//         image4.classList.add("image4");
-//         image5.classList.add("image5");
-//       } else {
-//         image1.classList.remove("image1");
-//         image2.classList.remove("image2");
-//         image3.classList.remove("image3");
-//         image4.classList.remove("image4");
-//         image5.classList.remove("image5");
-//       }
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        image1.style.transform = "translateX(0) translateY(0)";
+        image2.style.transform = "translateX(0) translateY(0)";
+        image3.style.transform = "translateX(0) translateY(0)";
+        image4.style.transform = "translateX(0) translateY(0)";
+        image5.style.transform = "translateX(0) translateY(0)";
+        image6.style.transform = "translateX(0) translateY(0)";
+        image7.style.transform = "translateX(0) translateY(0)";
+        image8.style.transform = "translateX(0) translateY(0)";
+      }
 
-//       // if (entry.isIntersecting) observer.unobserve(entry.target);
-//     });
-//   },
-//   {
-//     threshold: 0.5,
-//   }
-// );
+      // if (entry.isIntersecting) observer.unobserve(entry.target);
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
 
-// observer.observe(image1);
+observer.observe(image1);
 
-image1.onclick = () => {
-  image1.style.transform = "translateX(0) translateY(0)";
-  image2.style.transform = "translateX(0) translateY(0)";
-  image3.style.transform = "translateX(0) translateY(0)";
-  image4.style.transform = "translateX(0) translateY(0)";
-  image5.style.transform = "translateX(0) translateY(0)";
-  image6.style.transform = "translateX(0) translateY(0)";
-  image7.style.transform = "translateX(0) translateY(0)";
-  image8.style.transform = "translateX(0) translateY(0)";
-};
+// image1.onclick = () => {
+//   image1.style.transform = "translateX(0) translateY(0)";
+//   image2.style.transform = "translateX(0) translateY(0)";
+//   image3.style.transform = "translateX(0) translateY(0)";
+//   image4.style.transform = "translateX(0) translateY(0)";
+//   image5.style.transform = "translateX(0) translateY(0)";
+//   image6.style.transform = "translateX(0) translateY(0)";
+//   image7.style.transform = "translateX(0) translateY(0)";
+//   image8.style.transform = "translateX(0) translateY(0)";
+// };
